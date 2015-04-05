5baf050bbe1e06739ddd9f347ed0b0b1e0da74a3
Kent C. Dodds <kent+github@doddsfamily.us>
2015-04-04 22:41:05 -0600

    Using the for directive

--- src/person-card.js
+++ src/person-card.js
@@ -1,27 +1,47 @@
-import {Component, Template} from 'angular2/angular2';
+import {Component, Template, For} from 'angular2/angular2';
 
 @Component({
   selector: 'person-card'
 })
 @Template({
+  directives: [For],
   inline: `
     <div class="person-card">
       <div class="person-avatar-container">
         <img src="fred.png"/>
       </div>
       <div class="person-properties">
-        <div>
-          <strong>Name: </strong> Fred Mertz
-        </div>
-        <div>
-          <strong>Username: </strong> TheFredMertz
-        </div>
-        <div>
-          <strong>Email: </strong> freddie@aol.com
+        <div *for="#prop of properties">
+          <strong>{{prop.title}}: </strong> {{prop.getVal(person)}}
         </div>
       </div>
     </div>
   `
 })
 export class PersonCard {
+  constructor() {
+    this.person = {
+      name: {
+        first: 'Fred',
+        last: 'Mertz'
+      },
+      username: 'TheFredMertz',
+      email: 'freddie@aol.com'
+    };
+
+    this.properties = [
+      {
+        title: 'Name',
+        getVal: person => `${person.name.first} ${person.name.last}`
+      },
+      {
+        title: 'Username',
+        getVal: person => person.username
+      },
+      {
+        title: 'Email',
+        getVal: person => person.email
+      }
+    ];
+  }
 }
