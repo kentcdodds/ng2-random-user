bd47c3c9d820d703b4594c990ad54aad08138100
Kent C. Dodds <kent+github@doddsfamily.us>
2015-04-04 22:17:38 -0600

    Bootstrap simple component

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-alert('Hello World!');
+import {Component, Template, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'main'
+})
+@Template({
+  inline: `hello world!`
+})
+class Main {
+}
+
+bootstrap(Main);
