e5e9c6912323e7c2d66ba94c4390a30c77d81d31
Kent C. Dodds <kent+github@doddsfamily.us>
2015-04-06 09:08:36 -0600

    First component

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-alert('Hello Philly ETE!');
+import {Component, Template, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'main'
+})
+@Template({
+  inline: `<div>Hello Philly ETE!</div>`
+})
+class Main {
+}
+
+bootstrap(Main);
