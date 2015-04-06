f6c05ba8468094c13e780e63c003712d7abfde6e
Kent C. Dodds <kent+github@doddsfamily.us>
2015-04-06 09:10:04 -0600

    template curly bindings

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import {Component, Template, bootstrap} from 'angular2/angular2';
   selector: 'main'
 })
 @Template({
-  inline: `<div>Hello Philly ETE!</div>`
+  inline: `
+    <div>{{greeting}}</div>
+  `
 })
 class Main {
+  constructor() {
+    this.greeting = 'Hello Philly ETE! You rock!';
+  }
 }
 
 bootstrap(Main);
