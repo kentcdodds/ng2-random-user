d198d4060f081d78db7edcee0e62dfdd3c98828e
Kent C. Dodds <kent+github@doddsfamily.us>
2015-04-06 20:40:36 -0400

    Using components with one another

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-alert('Hello ETE!');
+import {Component, Template, bootstrap} from 'angular2/angular2';
+import {UserCard} from 'user-card';
+
+@Component({
+  selector: 'main'
+})
+@Template({
+  directives: [UserCard],
+  inline: `
+    <user-card></user-card>
+  `
+})
+class Main {
+}
+
+bootstrap(Main);
