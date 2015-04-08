6b94963db356928e994065aec9bc057e52d3b755
Kent C. Dodds <kent+github@doddsfamily.us>
2015-04-08 01:39:31 -0400

    Step 1

--- src/RandomUser.js
+++
@@ -1,7 +0,0 @@
-export class RandomUser {
-  getUser() {
-    var url = 'http://api.randomuser.me';
-    //var url = 'http://localhost:3000'; // just in case there's no internet...
-    return axios.get(url).then(response => response.data.results[0].user);
-  }
-}
--- src/main.js
+++ src/main.js
@@ -1,43 +1,14 @@
-import {Component, Template, bootstrap, If} from 'angular2/angular2';
-import {UserCard} from 'user-card';
-import {RandomUser} from 'RandomUser';
+import {Component, Template, bootstrap} from 'angular2/angular2';
 
 @Component({
-  selector: 'main',
-  services: [RandomUser]
+  selector: 'main'
 })
 @Template({
-  directives: [UserCard],
   inline: `
-    <div class="new-user-button">
-      <button class="ru-button --primary" autofocus (click)="getRandomUser()">
-        <i class="fa fa-user"></i>
-        {{buttonText}}
-      </button>
-    </div>
-    <user-card [user]="user" [loading]="loading">
-      <div loading>
-        <i class="fa fa-refresh fa-2x fa-spin"></i>
-      </div>
-      <div no-user>
-        <small>please click on "{{buttonText}}"</small>
-      </div>
-    </user-card>
+    <div>Hello Philly ETE</div>
   `
 })
-export class App {
-  constructor(randomUser:RandomUser) {
-    this.buttonText = 'Get New User';
-    this.getUser = randomUser.getUser;
-  }
-
-  getRandomUser() {
-    this.loading = true;
-    this.getUser().then(user => {
-      this.user = user;
-      this.loading = false;
-    }).catch(() => this.loading = false);
-  }
+class Main {
 }
 
-bootstrap(App);
+bootstrap(Main);
