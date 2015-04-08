ff65acb2c86a770069742ff0150474d205c84b54
Kent C. Dodds <kent+github@doddsfamily.us>
2015-04-08 01:44:40 -0400

    Using components in other components

---
+++ src/user-card.js
@@ -0,0 +1,13 @@
+import {Component, Template} from 'angular2/angular2';
+
+@Component({
+  selector: 'user-card'
+})
+@Template({
+  inline: `
+    <div class="">
+    </div>
+  `
+})
+export class UserCard {
+}
